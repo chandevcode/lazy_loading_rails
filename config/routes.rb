1445dcb1ceb9b568dd31cbb155e5cf75ba0bf11a
chan <baguschandradp@gmail.com>
2023-02-17 10:49:01 +0800

    starter

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :comments
+  root 'comments#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
